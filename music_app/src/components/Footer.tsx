2c25165ab34b226a11056da7012126ac7d4e5b72
Thiago <dejesusxavierthiago@gmail.com>
2024-11-02 14:50:48 -0300

    improved accessibility

--- music_app/src/components/Footer.tsx
+++ music_app/src/components/Footer.tsx
@@ -1,10 +1,10 @@
 export function Footer() {
     return (
-    <footer className='text-blue-500 p-4 sm:px-3 sm:py-2 sm:text-xs flex-grow-0-0'>
+    <footer className='text-blue-400 p-4 sm:px-3 sm:py-2 sm:text-xs flex-grow-0-0'>
         <p>
             Developed by 
-            <span className="font-bold uppercase text-blue-700">Thiago
-                <span className="uppercase text-blue-500">dev</span>
+            <span className="font-bold uppercase text-blue-500">Thiago
+                <span className="uppercase text-blue-400">dev</span>
             </span>
         </p>
     </footer>
